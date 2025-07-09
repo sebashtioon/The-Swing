7eadb03a4970885e53e2892dd8497d447315ec12
sebashtioon <sebastiansuciu607@gmail.com>
2025-07-09 17:24:01 +0300

    Add input handling for mouse capture and visibility; implement new shaders for visual effects

--- src/autoload/master_input.gd
+++ src/autoload/master_input.gd
@@ -1,6 +1,12 @@
 extends Node
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if OS.is_debug_build():
 		if Input.is_action_just_pressed("quit"):
 			get_tree().quit()
+		
+		if Input.is_action_just_pressed("capture_mouse"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+		if Input.is_action_just_pressed("show_mouse"):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
