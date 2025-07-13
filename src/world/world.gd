5a9e9ac7ceba72f9f96e8ae339e9dc5f5863e9db
sebashtioon <sebastiansuciu607@gmail.com>
2025-07-13 23:05:05 +0300

    Adjust timer and camera behavior on game start
    Reduced FigureTimer wait time from 60 to 3 seconds and updated figure transform in world.tscn. On game start, TitleScreenLayer is now hidden and mouse mode is captured when sitting down for improved user experience.

--- src/world/world.gd
+++ src/world/world.gd
@@ -34,6 +34,7 @@ func title_screen():
 func start_game():
 	can_blink = true
 	swing.swing()
+	$camera_pos/camera_pivot/main_camera/TitleScreenLayer.hide()
 	$camera_pos/camera_pivot/main_camera.make_current()
 	dialogue_timeline.play(&"main")
 	$"non-physical/FigureTimer".start()
@@ -130,6 +131,7 @@ func _on_leave_button_mouse_exited() -> void:
 
 
 func _on_sit_down_button_pressed() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$camera_pos/camera_pivot/main_camera/OverlayLayer/BlackOverlay.modulate = Color(1, 1, 1, 0)
 	$camera_pos/camera_pivot/main_camera/OverlayLayer/BlackOverlay.show()
 	var tween = get_tree().create_tween()
